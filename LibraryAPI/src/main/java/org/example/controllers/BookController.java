370f5a7c49cee2b9d9fecfa01262cf929d4bb265
Faroferinho <conrado.perini.fracacio@gmail.com>
2025-05-17 15:41:53 -0300

    Minor bug fixes

--- LibraryAPI/src/main/java/org/example/controllers/BookController.java
+++ LibraryAPI/src/main/java/org/example/controllers/BookController.java
@@ -1,5 +1,6 @@
 package org.example.controllers;
 
+import org.apache.tomcat.util.bcel.Const;
 import org.example.constants.Constant;
 import org.example.documents.dto.BookDTO;
 import org.example.documents.media.Book;
@@ -8,14 +9,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
-@Controller
+@RestController
 public class BookController implements CRUDThymeleaf<Book, BookDTO> {
     @Autowired
     private BookService service;
@@ -28,16 +27,19 @@ public class BookController implements CRUDThymeleaf<Book, BookDTO> {
     }
 
     @Override
+    @GetMapping(Constant.API_BOOK)
     public ResponseEntity<List<Book>> findAll() {
         return ResponseEntity.ok(service.findAll());
     }
 
     @Override
+    @GetMapping(Constant.API_BOOK + "/{id}")
     public ResponseEntity<Optional<Book>> findById(@PathVariable("id") String id) {
         return ResponseEntity.ok(service.findById(id));
     }
 
     @Override
+    @DeleteMapping(Constant.API_BOOK + "/{id}")
     public ResponseEntity<Void> deleteById(@PathVariable("id") String id) {
         service.deleteById(id);
         return ResponseEntity.noContent().build();
